545091111e3f5dbfca3c841cc5b6ab9f44a4f382
ptitball <ptitball69@gmail.com>
2025-11-15 04:25:22 -0800

    Create servo_controller.cpp

---
+++ arduino/servo_controller.cpp
@@ -0,0 +1,9 @@
+#include "servo_controller.hpp"
+
+void ServoController::setup() {
+    // Initialize servos here
+}
+
+void ServoController::setPosition(int servoIndex, int angle, int speed) {
+    // Set servo to angle at speed
+}
